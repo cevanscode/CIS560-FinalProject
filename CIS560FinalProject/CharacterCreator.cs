2bfd716b55d7bf733c7df70db3b2b437a7018ef9
KasenFudge <kasenf@ksu.edu>
2024-11-13 13:14:43 -0600

    Renaming and Adding Some Views For Basic Stuff

--- CIS560FinalProject/CharacterCreator.cs
+++ CIS560FinalProject/CharacterCreator.cs
@@ -1,8 +1,8 @@
 namespace CIS560FinalProject
 {
-    public partial class Form1 : Form
+    public partial class CharacterCreator : Form
     {
-        public Form1()
+        public CharacterCreator()
         {
             InitializeComponent();
         }
