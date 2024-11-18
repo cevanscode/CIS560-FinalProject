7e7e753a1e11f8be5fd8880e647fe4613bbde0be
matthewkelley22 <matthewkelley22@ksu.edu>
2024-11-18 15:19:58 -0600

    Save progress

--- CIS560FinalProject/SQL/Procedures.sql
+++ CIS560FinalProject/SQL/Procedures.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS ShowSubclassPopularity;
 GO
 
-CREATE VIEW ShowSubclassPopularity
+CREATE VIEW SubclassPopularity
 AS
 SELECT S.Name, COUNT(CS.SubclassID) AS SubclassCount
 FROM CharacterSubclass CS INNER JOIN
@@ -10,4 +10,10 @@ ORDER BY COUNT(CS.SubclassID) DESC;
 GO
 
 GRANT SELECT ON VIEW::ShowSubclassPopularity TO PUBLIC;
+GO
+
+CREATE VIEW MostChosenTalents
+AS
+SELECT S.Name, 
+GROUP BY S.ClassID
 GO
